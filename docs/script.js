14760f6605501d626a4d2c35dd518e6da87dcd7f
Onedog-Andre <77590571+Onedog-Andre@users.noreply.github.com>
2021-07-07 11:04:18 -0400

    icones

--- docs/script.js
+++ docs/script.js
@@ -11,6 +11,18 @@ window.addEventListener('scroll', () => {
 const darkModeBtn = document.querySelector("#theme-btn");
 
 darkModeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme")
+  document.body.classList.toggle("light-theme")
 });
+
+// menu-animation
+const navBtn = document.getElementById("menu-btn-container");
+navBtn.addEventListener("click", () =>{
+    document.querySelector('.menu-btn').classList.toggle('menu-btn-open')
+    document.querySelector('.links-container').classList.toggle('links-container-open')
+})
+
+const temaBtn = document.querySelector(".tema-container");
+temaBtn.addEventListener("click", () =>{
+   temaBtn.classList.toggle('tema-container-on');
+})
 
